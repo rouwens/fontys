734262005986e3bc77762c58de0f6deb1406f3a5
rouwens <drouwens@gmail.com>
2021-02-23 14:34:17 +0100

    Create Availability checker.py

---
+++ Availability checker.py
@@ -0,0 +1,24 @@
+import os
+import time
+
+def func_ping():
+    timer = 5
+    ping = os.system("ping -c 1 rpi.lani")
+    date = os.system("date >> ping.log")
+
+    if ping == 0:
+        with open("ping.log", "a") as ping_log:
+            ping_log.write("Host is bereikbaar")
+            date
+            ping_log.write("\n")
+
+    else:
+        with open("ping.log", "a") as ping_log:
+            ping_log.write("Host is niet bereikbaar")
+            date
+            ping_log.write("\n")
+
+    time.sleep (timer)
+    func_ping()
+
+func_ping()
