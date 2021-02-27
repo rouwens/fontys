4fa4e24124daecd3d7609ada7eccc2c0c0aafea9
rouwens <drouwens@gmail.com>
2021-02-27 21:49:17 +0100

    Bijgewerkt

--- ifconfig humanfriendly format/import.bash
+++ ifconfig humanfriendly format/import.bash
@@ -2,3 +2,4 @@ rm info.txt
 ifconfig $1 | grep inet | awk '$1 == "inet" { print $2 }' >> info.txt
 ifconfig $1 | grep inet | awk '$1 == "inet" { print $4 }' >> info.txt
 ip r | grep default | awk '$1 == "default" { print $3 }' >> info.txt
+ifconfig $1 | grep ether | awk '$1 == "ether" { print $2 }' >> info.txt
