5f6d0626827a66afa3f4db18b4ef6430f9c72841
rouwens <drouwens@gmail.com>
2021-03-05 21:57:58 +0100

    Bijgewerkt

--- test.py
+++ test.py
@@ -1,6 +1,3 @@
-import subprocess
-
-text = input()
-ping = subprocess.check_output (['echo', text])
-print (ping)
-
+import pyad
+x = pyad.set_defaults(ldap_server="192.168.195.123", username="cn=Administrator,cn=Users,dc=rouwens,dc=com", password="W3lk0m!")
+print (x)
