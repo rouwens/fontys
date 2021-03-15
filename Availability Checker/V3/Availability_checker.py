fae4b2ec8c0b3e657f0ee84a2f1a6644ac4f7572
rouwens <drouwens@gmail.com>
2021-03-15 19:45:27 +0100

    Bijgewerkt

--- Availability Checker/V3/Availability_checker.py
+++ Availability Checker/V3/Availability_checker.py
@@ -2,40 +2,140 @@ import os
 import time
 import mysql.connector as mysql
 import datetime
+import time
+
+def start():
+
+    def checker():
+        def func_ping(url, timer):
+
+            een = "1"
+            nul = "0"
+
+            inttimer = int(timer)
+            timer = inttimer
+            ping = os.system("ping -c 1 {0}".format(url))
+            
+            date_full = datetime.datetime.now()
+            date = date_full.strftime("%x")
+
+            db = mysql.connect(
+                host = "rouwens.ddns.net",
+                user = "drouwens-remote",
+                passwd = "Daan04092000.",
+                database = "availability-checker"
+            )
+            cursor = db.cursor()
+
+            if ping == 0:
+                cursor.execute("INSERT INTO data VALUES (NULL, %s, %s, %s, %s)", (een, nul, url, date))
+                db.commit()
+
+            else:
+                cursor.execute("INSERT INTO data VALUES (NULL, %s, %s, %s, %s)", (nul, een, url, date))
+                db.commit()
+
+            time.sleep (timer)
+            func_ping(url, timer)
+
+        print ("")
+        print ("Vul hier de URL in van de website die je wilt checken op de beschikbaarheid")
+        url = input()
+
+        print ()
+        print ("Hoeveel seconde moet er tussen de pings zitten?")
+        timer = input()
+
+        func_ping(url, timer)
+
+    def resultaat():
+            
+        db = mysql.connect(
+            host = "192.168.178.254",
+            user = "drouwens-remote",
+            passwd = "Daan04092000.",
+            database = "availability-checker"
+                )
+        cursor = db.cursor()
+
+        def sqlgelukt(url, strdatum):
+
+            querygelukt = """SELECT SUM(gelukt) FROM data WHERE url = %s AND datum = %s"""
+            cursor.execute(querygelukt, (url, strdatum))
+            records = cursor.fetchall()
+            for record in records:
+                var = record
+                
+            varstr = str(var)
+            indexvoor = varstr.index("(") + 1
+            indexachter = varstr.index(",") -2
+            uitslag = varstr[indexvoor:indexachter]
+            print ("Aantal gelukte pings:", uitslag)
+
+        def sqlmislukt(url, strdatum):
+
+            querymislukt = """SELECT SUM(mislukt) FROM data WHERE url = %s AND datum = %s"""
+            cursor.execute(querymislukt, (url, strdatum))
+            records = cursor.fetchall()
+            for record in records:
+                var = record
+
+            varstr = str(var)
+            indexvoor = varstr.index("(") + 1
+            indexachter = varstr.index(",") -2
+            uitslag = varstr[indexvoor:indexachter] 
+            print ("Aantal mislukte pings:", uitslag)
+        
+        print ("")
+        print ("Vul de URL in waarvan je de resultaten wilt zien.")
+        url = input()
+
+        date_full = datetime.datetime.now()
+        date = date_full.strftime("%x")
+
+        print ("")
+        print ("Vul hier de datum waarvan de wilt terug kijken van de ping uitslagen")
+        print ("Gebruik hierbij de volgende tijdsnotatie maand/dag/jaar. Datum van vandaag is: " + date )
+        datum = input()
+        strdatum = str(datum)
+        print ()
+
+        sqlgelukt(url, strdatum)
+        sqlmislukt(url, strdatum)
+
+        print ()
+        print ("Nieuwe resultaten bekijken? (j/n)")
+        nogeenkeer = input()
+        
+        if nogeenkeer == "j":
+            resultaat()
+        
+        if nogeenkeer == "n":
+            start()
 
 
-def func_ping(url):
+    print ("")
+    print ("Toets één van de volgende opties in")
+    print ("")
+    print ("1 - Website/systeem pingen")
+    print ("2 - Resultaten bekijken")
+    print()
+    print ("3 - stoppen")
+    print ("")
 
-    een = "1"
-    nul = "0"
-    timer = 5
-    ping = os.system("ping -c 1 {0}".format(url))
+    keuze = input()
+
+    if keuze == "1":
+        checker()
+
+    if keuze == "2":
+        resultaat()
     
-    date_full = datetime.datetime.now()
-    date = date_full.strftime("%x")
-
-    db = mysql.connect(
-        host = "rouwens.ddns.net",
-        user = "drouwens-remote",
-        passwd = "Daan04092000.",
-        database = "availability-checker"
-    )
-    cursor = db.cursor()
-
-    if ping == 0:
-        cursor.execute("INSERT INTO data VALUES (NULL, %s, %s, %s, %s)", (een, nul, url, date))
-        db.commit()
+    if keuze == "3":
+        exit()
 
     else:
-        cursor.execute("INSERT INTO data VALUES (NULL, %s, %s, %s, %s)", (nul, een, url, date))
-        db.commit()
-
-    time.sleep (timer)
-    func_ping(url)
-
-
-print ("Vul hier de URL in van de website die je wilt checken op de beschikbaarheid")
-url = input()
-func_ping(url)
-
-
+        print ("Verkeerde input. Probeer het opnieuw....")
+        time.sleep(3)
+        start()
+start()
--- test.py
+++ test.py
@@ -1,11 +1,19 @@
-def getPerson(name, age, country):
-    name = "leeg"
-    age = 0
-    country = "leeg"
-    return name,age,country
+#def getPerson(name, age, country):
+ #   name = "leeg"
+  #  age = 0
+   # country = "leeg"
+    #return name,age,country
 
-getPerson (name = "Daan", age = 20, country = "NL")
-name,age,country = getPerson()
-print(name)
-print(age)
-print(country)
+#getPerson (name = "Daan", age = 20, country = "NL")
+#name,age,country = getPerson()
+#print(name)
+#print(age)
+#print(country)
+
+import discord
+var = "ja"
+
+if var == "ja":
+    test = client.get_channel()
+
+    await message.test.send("Test123...")
