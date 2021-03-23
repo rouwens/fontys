54100d0bae7c1833224da5d98c0318f17ecef20e
rouwens <57994396+rouwens@users.noreply.github.com>
2021-03-23 15:56:36 +0100

    Bijgewerkt

--- Spammer/spammer.py
+++ Spammer/spammer.py
@@ -1,16 +1,41 @@
 import smtplib
+import configparser
+from email.mime.text import MIMEText
+#from email.mime.multipart import MIMEMultipar
 
-to = 'daan@daanesmee.nl'
-gmail_user = 'daan@daanesmee.nl'
-gmail_pwd = 'Daan0409.'
-smtpserver = smtplib.SMTP('mail.daanesmee.nl',587)
+# Importeren va config bestand
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+# Mail server instellingen
+smtp_server = 'mail.daanesmee.nl'
+smtp_port = 587
+smtp_username = 'spam@daanesmee.nl'
+smtp_password = 'Spammer123.'
+
+# Bericht informatie
+subject = config['mail']['subject']
+
+# Bericht in HTML
+f = open('spam.html', 'r')
+html = f.read()
+htmlstage = MIMEText(html, "html")
+message = str(htmlstage)
+
+receivers_mail = ['slachtoffer1@daanesmee.nl', 'daan@daanesmee.nl']
+to = ", ".join(receivers_mail)
+#to = ['slachtoffer1@daanesmee.nl', 'daan@daanesmee.nl']
+#to = ", ".join(recipients)
+
+smtp_subject = 'Subject:' + subject + '\n'
+smpt_sender = smtp_username
+smtp_sender_pwd = smtp_password
+smtpserver = smtplib.SMTP(smtp_server,smtp_port)
 smtpserver.ehlo()
 smtpserver.starttls()
 smtpserver.ehlo
-smtpserver.login(gmail_user, gmail_pwd)
-header = 'To:' + to + '\n' + 'From: ' + gmail_user + '\n' + 'Subject:testing \n'
-print (header)
-msg = header + '\n this is test msg from mkyong.com \n\n'
-smtpserver.sendmail(gmail_user, to, msg)
-#print 'done!'
+smtpserver.login(smpt_sender, smtp_sender_pwd)
+header = 'To:' + to + '\n' + 'From: ' + smpt_sender + '\n' + smtp_subject
+msg = header + message
+smtpserver.sendmail(smpt_sender, to, msg)
 smtpserver.close()
