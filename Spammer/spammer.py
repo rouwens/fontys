867d56ca391fdc0a47355dd10ce485f98b64daac
rouwens <57994396+rouwens@users.noreply.github.com>
2021-03-30 15:41:12 +0200

    Bijgewerkt

--- Spammer/spammer.py
+++ Spammer/spammer.py
@@ -1,20 +1,6 @@
 import smtplib
-import configparser
 from email.mime.text import MIMEText
-#from email.mime.multipart import MIMEMultipar
-
-# Importeren va config bestand
-config = configparser.ConfigParser()
-config.read('config.ini')
-
-# Mail server instellingen
-smtp_server = 'mail.daanesmee.nl'
-smtp_port = 587
-smtp_username = 'spam@daanesmee.nl'
-smtp_password = 'Spammer123.'
-
-# Bericht informatie
-subject = config['mail']['subject']
+from email.mime.multipart import MIMEMultipart
 
 # Bericht in HTML
 f = open('spam.html', 'r')
@@ -22,20 +8,26 @@ html = f.read()
 htmlstage = MIMEText(html, "html")
 message = str(htmlstage)
 
-receivers_mail = ['daan@daanesmee.nl', 'slachtoffer1@daanesmee.nl']
-to = ", ".join(receivers_mail)
-#to = ['slachtoffer1@daanesmee.nl', 'daan@daanesmee.nl']
-#to = ", ".join(recipients)
 
-smtp_subject = 'Subject:' + subject + '\n'
-smpt_sender = smtp_username
-smtp_sender_pwd = smtp_password
-smtpserver = smtplib.SMTP(smtp_server,smtp_port)
+# Verzendlijst
+to = ['daan@daanesmee.nl', 'slachtoffer1@daanesmee.nl', 'slachtoffer2@daanesmee.nl' , 'slachtoffer3@daanesmee.nl' , 'slachtoffer4@daanesmee.nl', 'esmee@daanesmee.nl']
+
+# Server connectie
+smtp_user = 'spam@daanesmee.nl'
+smtp_pwd = 'Spammer123.'
+smtpserver = smtplib.SMTP("mail.daanesmee.nl",587)
 smtpserver.ehlo()
 smtpserver.starttls()
 smtpserver.ehlo
-smtpserver.login(smpt_sender, smtp_sender_pwd)
-header = 'To:' + to + '\n' + 'From: ' + smpt_sender + '\n' + smtp_subject
-msg = header + message
-smtpserver.sendmail(smpt_sender, to, msg)
+smtpserver.login(smtp_user, smtp_pwd)
+
+# Mail opstellen
+msg = MIMEMultipart('alternative')
+msg.attach(htmlstage)
+msg['Subject'] = "Dit is spam"
+msg['From'] = smtp_user
+msg['To'] = ", ".join(to)
+
+# Mail verzenden en connectie verbreken
+smtpserver.sendmail(smtp_user, to, msg.as_string())
 smtpserver.close()
